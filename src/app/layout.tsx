37535f70550e1ac10c17abf035d2f245c99f3aaa
donovancastaneda <donovandcastanedajr@gmail.com>
2024-03-17 15:27:29 -0500

    fix: background mobile modal issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <div vaul-drawer-wrapper="" className="bg-background">
+        <div>
           {children}
         </div>{" "}
         <Footer />
