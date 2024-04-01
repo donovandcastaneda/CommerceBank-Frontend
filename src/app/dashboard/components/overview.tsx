008d010215a968aa59986f8296e65ce9736098ae
donovancastaneda <donovandcastanedajr@gmail.com>
2024-04-01 17:04:34 -0500

    feat: made dashboard based off balance and total withdrawn

--- src/app/dashboard/components/overview.tsx
+++ src/app/dashboard/components/overview.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import {
   Bar,
   BarChart,
@@ -11,8 +14,6 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-
-
 
 const data = [
   {
@@ -79,10 +80,74 @@ const data = [
 ];
 
 export function Overview() {
+  type UserData = {
+    firstName: string;
+    lastName: string;
+    balance: number;
+    totalDeposited: number;
+    totalWithdrawn: number;
+  };
+
+  type GraphData = {
+    name: string;
+    total: number;
+    expenses: number;
+  };
+
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [graphData, setGraphData] = useState<GraphData[]>([]); 
+
+  let user = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/users/` + user.id
+        );
+        console.log(response);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [user.id]);
+
+  useEffect(() => {
+    if (userData) {
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      const newData = months.map((month) => {
+        const totalFluctuation = 0.9 + Math.random();
+        const expensesFluctuation = 0.9 + Math.random();
+
+        return {
+          name: month,
+          total: (userData.balance / 12) * totalFluctuation,
+          expenses: (userData.totalWithdrawn / 12) * expensesFluctuation,
+        };
+      });
+      setGraphData(newData);
+    }
+  }, [userData]);
+
   return (
-    
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
+      <BarChart data={graphData}>
         <XAxis
           dataKey="name"
           stroke="#888888"
@@ -97,7 +162,7 @@ export function Overview() {
           axisLine={false}
           tickFormatter={(value: any) => `$${value}`}
         />
-          <Legend />
+        <Legend />
 
         <Bar
           dataKey="total"
@@ -105,7 +170,7 @@ export function Overview() {
           radius={[4, 4, 0, 0]}
           className="fill-primary"
         />
-         <Bar
+        <Bar
           dataKey="expenses"
           fill=""
           radius={[4, 4, 0, 0]}
