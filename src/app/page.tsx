10b8e2c29c48702e2627e4209f311688a7d9f06b
donovancastaneda <donovandcastanedajr@gmail.com>
2024-04-22 16:35:27 -0500

    fix: got rid of user profile component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import SignInForm from "@/components/SignInForm";
-import UserProfile from "@/components/UserProfile";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
